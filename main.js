2492dd7812608b37e4f02e6ab3817048f9bfda9e
agustin <agustinaleks@gmail.com>
2021-02-24 15:52:28 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function generateTime() {
+ 
+    let timeNow = new Date();
+  
+    let hours = timeNow.getHours().toString().length < 2 ? "0" + timeNow.getHours() : timeNow.getHours();
+    let minutes = timeNow.getMinutes().toString().length < 2 ? "0" + timeNow.getMinutes() : timeNow.getMinutes();
+    let seconds = timeNow.getSeconds().toString().length < 2 ? "0" + timeNow.getSeconds() : timeNow.getSeconds();
+
+  
+    let mainTime = `${hours}:${minutes}:${seconds}`;
+    
+  
+}
+
+
+setInterval(() => {
+    generateTime();
+}, 1000);
+
+
